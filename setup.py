f9d1293cf35eeeba56bbf9e2d5755853cf4fa203
Penny Wieser <penny.wieser@gmail.com>
2021-08-10 10:33:30 -0700

    added set up

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-setuptools.setup(
+setup(
     name="Thermobar",
     version=__version__,
     author="Penny, Maurizio, Jordan, Eric",
